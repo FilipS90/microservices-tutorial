771438f61d90eb39274ee05771781adb488c85d6
FilipS90 <filip.stojiljkovic90@yahoo.com>
2023-05-03 20:14:23 +0200

    removing id from OrderLineItemsDto

--- order-service/src/main/java/com/filips90/orderservice/dto/OrderLineItemsDto.java
+++ order-service/src/main/java/com/filips90/orderservice/dto/OrderLineItemsDto.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderLineItemsDto {
-	private Long id;
 	private String skuCode;
 	private BigDecimal price;
 	private Integer quantity;
